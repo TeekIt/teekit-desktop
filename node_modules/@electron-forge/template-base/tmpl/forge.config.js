fed3d7d43442754811fa30f5c0213710d23041d7
Muhammad Abdullah Mirza <abdullah.izhar@teo-intl.com>
2024-07-14 01:09:57 +0500

    Teek it desktop app released

---
+++ node_modules/@electron-forge/template-base/tmpl/forge.config.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5414a6b629ecad5022698cb1694fe92dcf28b39afd0116951c595d0e80eab64f
+size 1172
---
+++ node_modules/@electron-forge/template-base/tmpl/index.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a95eeb85b8196df8e95c7405ca7296cf73270614b410204744ca47708e3b310a
+size 1514
